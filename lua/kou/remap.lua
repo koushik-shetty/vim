813df26a0ba65390a4cde1b9d3453a8f30c10200
Super User <root@KouHomeWin>
2023-07-11 10:42:33 +0530

    add remap for paste without copying deleted

--- lua/kou/remap.lua
+++ lua/kou/remap.lua
@@ -4,3 +4,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "<C-J>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<C-K>", ":m '<-2<CR>gv=gv")
 
+--" delete without yanking
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
+
+--" replace currently selected text with default register
+--"-" without yanking it
+vim.keymap.set("v", "<leader>p", '"_dP')
